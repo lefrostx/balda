11822f06f4a5348a56b77a44f245548fdd97fd0f
lefrostx <lefrostx@gmail.com>
2016-08-03 03:52:04 +0500

    Добавлены тесты на проверку буквы в ячейке поля

--- BaldaLogicTest/tst_baldalogictest.cpp
+++ BaldaLogicTest/tst_baldalogictest.cpp
@@ -13,41 +13,63 @@ public:
 
 private:
     GameBalda::GameLogic logic{"baldafile.txt"};
+    ClarensMath::Matrix<QChar> gameArena{5, 5, ' '};
 
 private Q_SLOTS:
     void testCorrectResult();
-    void testFreeCell();
+    void testFreeCellTrue();
+    void testFreeCellFalse();
+    void testNearIsLetterTrue();
+    void testNearIsLetterFalse();
+    void testCellInRangeTrue();
+    void testCellInRangeFalse();
 };
 
 BaldaLogicTest::BaldaLogicTest()
 {
-}
-
-void BaldaLogicTest::testCorrectResult()
-{
-    ClarensMath::Matrix<QChar> gameArena{5, 5, ' '};
     gameArena(2, 0) = 'Б';
     gameArena(2, 1) = 'А';
     gameArena(2, 2) = 'Л';
     gameArena(2, 3) = 'Д';
     gameArena(2, 4) = 'А';
+}
 
+void BaldaLogicTest::testCorrectResult()
+{
     QStringList actualList = logic.findWordsList(gameArena);
     QStringList expectList{"ФАЛДА", ""};
 
     QCOMPARE(actualList, expectList);
 }
 
-void BaldaLogicTest::testFreeCell()
+void BaldaLogicTest::testFreeCellTrue()
 {
-    ClarensMath::Matrix<QChar> gameArena{5, 5, ' '};
-    gameArena(2, 0) = 'Б';
-    gameArena(2, 1) = 'А';
-    gameArena(2, 2) = 'Л';
-    gameArena(2, 3) = 'Д';
-    gameArena(2, 4) = 'А';
-
     QVERIFY(logic.isFreeCell(gameArena, 0, 0));
+}
+
+void BaldaLogicTest::testFreeCellFalse()
+{
+    QVERIFY(!logic.isFreeCell(gameArena, 2, 0));
+}
+
+void BaldaLogicTest::testNearIsLetterTrue()
+{
+    QVERIFY(logic.isNearLetter(gameArena, 1, 1));
+}
+
+void BaldaLogicTest::testNearIsLetterFalse()
+{
+    QVERIFY(!logic.isNearLetter(gameArena, 4, 2));
+}
+
+void BaldaLogicTest::testCellInRangeTrue()
+{
+    QVERIFY(logic.isInRange(gameArena, 1, 1));
+}
+
+void BaldaLogicTest::testCellInRangeFalse()
+{
+    QVERIFY(!logic.isInRange(gameArena, 5, 5));
 }
 
 QTEST_APPLESS_MAIN(BaldaLogicTest)
