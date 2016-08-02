04c44234e3611b59dccd7be97be0c0451da41508
lefrostx <lefrostx@gmail.com>
2016-08-03 02:06:55 +0500

    Добавлен функционал на проверку клетки игрового поля на свободность

--- Balda/gamelogic.cpp
+++ Balda/gamelogic.cpp
@@ -8,6 +8,24 @@ GameBalda::GameLogic::GameLogic(const QString &filename) :
 
 QStringList GameBalda::GameLogic::findWordsList(const ClarensMath::Matrix<QChar> &arena) const
 {
+    for (int row = 0; row < arena.rows(); ++row) {
+        for (int col = 0; col < arena.cols(); ++col) {
+
+        }
+    }
+
+
     QStringList lst{"ФАЛДА", ""};
     return lst;
+}
+
+bool GameBalda::GameLogic::isFreeCell(const ClarensMath::Matrix<QChar> &arena, int row, int col) const
+{
+    if (row < 0 || row >= arena.rows())
+        return false;
+
+    if (col < 0 || col >= arena.cols())
+        return false;
+
+    return arena(row, col) == ' ';
 }
--- Balda/gamelogic.h
+++ Balda/gamelogic.h
@@ -4,15 +4,22 @@
 #include <QStringList>
 #include "matrix.h"
 
+class BaldaLogicTest;
+
 namespace GameBalda {
 
     class GameLogic {
+        friend BaldaLogicTest;
     public:
         GameLogic(const QString & filename);
         QStringList findWordsList(const ClarensMath::Matrix<QChar> & arena) const;
 
     private:
+        bool isFreeCell(const ClarensMath::Matrix<QChar> & arena, int row, int col) const;
+
         QStringList fileWords;
+
+
     };
 
 }
